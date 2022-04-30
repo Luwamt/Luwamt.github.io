ffad2f3fa2f8393cd078b65531484b8d9c2f4c86
Luwam15 <80141128+Luwam15@users.noreply.github.com>
2022-04-30 00:53:03 -0500

    updated

--- javascript/tip.js
+++ javascript/tip.js
@@ -1,4 +1,9 @@
 function calcTip(){
-    let subtotal1=document.getElementById("subtotal")
+    
+        let subtotal1=document.getElementById("subtotal").value;
+        let tip2=document.getElementById("tip").value;
+       
 
+        document.getElementById("total").innerHTML= Number(subtotal1* Number(tip2/100))+ Number(subtotal1);
+    
 }
