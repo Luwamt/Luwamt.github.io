0027544df74701cab350829416e3d5da253417c5
Luwam15 <80141128+Luwam15@users.noreply.github.com>
2022-04-29 21:01:02 -0500

    task and tip cal added

---
+++ javascript/practice.js
@@ -0,0 +1,11 @@
+let count = 10;
+let s1 = "" + count;
+function add(){
+    return 2;
+}
+function display(){
+    console.log("hi fikir");
+}
+console.log(s1);
+console.log(add());
+console.log(display());
