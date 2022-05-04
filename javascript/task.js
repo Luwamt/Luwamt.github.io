56fb2da95907e2e030f05b27c58ed121f477d81d
Luwam15 <80141128+Luwam15@users.noreply.github.com>
2022-05-04 04:30:35 -0500

    upd

--- javascript/task.js
+++ javascript/task.js
@@ -1,29 +1,63 @@
 
-window.onload.fun;
+window.onload = onLoadEvent;
 
-function fun(){
-var addbtn=document.getElementById("task1").value;
-var clearbtn=document
+function onLoadEvent() {
 
-function addTask(){
-    let add= document.getElementById("task1").value;
-    let textArea=document.getElementById("rightbox").value;
+    var addBtn = document.getElementById("addBtn");
+    var clearBTn = document.getElementById("clearBtn");
+    //let mainTask = document.getElementById("textField");
+    //let task = document.getElementById("task");
 
-localStorage.setItem(add,textArea);
+    //mainTask.textContent += localStorage.getItem("name1");
+    //if (localStorage.getItem("name1") === null) mainTask.textContent = "";
 
-document.getElementById("task1").value=" ";
-document.getElementById("rightbox").value=""
+    //addBtn.onclick = 
+    addBtn.onclick = function  addingTask() {
 
+        //document.getElementById("textField").innerHTML += mainTask;
+        var taskKey = document.getElementById("task").value;
+        var mainTask = document.getElementById("textField");
 
+        //if (oldTask === null) oldTask = "";
+        //saving the user input
+        localStorage.setItem( taskKey,taskKey);
+        var storedKey = localStorage.getItem(taskKey);
+        console.log(taskKey);
 
-
+        document.getElementById("textField").value += storedKey+ "\n";
+        mainTask.textContent = localStorage.getItem(storedKey);
+        // document.getElementById("task").value = "";
+    }
+    // clearBTn.onclick = 
+    //clearing given task
+    clearBTn.onclick = function clearTask() {
+        localStorage.clear();
+        document.getElementById("task").value = "";
+        document.getElementById("textField").value = "";
+    }
 }
+// function fun(){
+// var addbtn=document.getElementById("task1").value;
+// var clearbtn=document
 
-function clearTask(){
-    localStorage.clear();
+// function addTask(){
+//     let add= document.getElementById("task1").value;
+//     let textArea=document.getElementById("rightbox").value;
 
-    document.getElementById("task1").value=" ";
-    document.getElementById("rightbox").value=""
+// localStorage.setItem(add,textArea);
+
+// document.getElementById("task1").value=" ";
+// document.getElementById("rightbox").value="";
+
+// }
+
+
+
+
+// function clearTask(){
+//     localStorage.clear();
+
+//     document.getElementById("task1").value=" ";
+//     document.getElementById("rightbox").value=""
     
-}
-}
+// 
