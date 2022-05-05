51285afb55f096ed055480ae695c645330916997
Luwam15 <80141128+Luwam15@users.noreply.github.com>
2022-05-05 01:18:18 -0500

    Updated

--- javascript/task.js
+++ javascript/task.js
@@ -1,41 +1,64 @@
+//Rob
 
 window.onload = onLoadEvent;
-
 function onLoadEvent() {
+  let task = document.getElementById("task");
+  let addBtn = document.getElementById("addBtn");
+  let clearBTn = document.getElementById("clearBtn");
+  let mainTask = document.getElementById("textField");
 
-    var addBtn = document.getElementById("addBtn");
-    var clearBTn = document.getElementById("clearBtn");
-    //let mainTask = document.getElementById("textField");
-    //let task = document.getElementById("task");
+  if (task) {
+    mainTask.textContent = localStorage.getItem('textinput');
+  }
 
-    //mainTask.textContent += localStorage.getItem("name1");
-    //if (localStorage.getItem("name1") === null) mainTask.textContent = "";
-
-    //addBtn.onclick = 
-    addBtn.onclick = function  addingTask() {
-
-        //document.getElementById("textField").innerHTML += mainTask;
-        var taskKey = document.getElementById("task").value;
-        var mainTask = document.getElementById("textField");
-
-        //if (oldTask === null) oldTask = "";
-        //saving the user input
-        localStorage.setItem( taskKey,taskKey);
-        var storedKey = localStorage.getItem(taskKey);
-        console.log(taskKey);
-
-        document.getElementById("textField").value += storedKey+ "\n";
-        mainTask.textContent = localStorage.getItem(storedKey);
-        // document.getElementById("task").value = "";
+  addBtn.onclick = function () {
+    //document.getElementById("textField").innerHTML += mainTask;
+    if (localStorage.getItem("textinput") && task.value != null) {
+      localStorage.setItem( "textinput", localStorage.getItem("textinput") + "\n" + task.value
+      );
+    } else {
+      localStorage.setItem("textinput", task.value);
     }
-    // clearBTn.onclick = 
-    //clearing given task
-    clearBTn.onclick = function clearTask() {
-        localStorage.clear();
-        document.getElementById("task").value = "";
-        document.getElementById("textField").value = "";
+    if (localStorage.getItem("textinput")) {
+      mainTask.textContent = localStorage.getItem("textinput");
     }
+  };
+  clearBTn.onclick = function () {
+    localStorage.clear();
+    document.getElementById("task").value = "";
+    document.getElementById("textField").value = "";
+  };
 }
+
+// window.onload = onLoadEvent;
+
+// function onLoadEvent() {
+
+//     var addBtn = document.getElementById("addBtn");
+//     var clearBTn = document.getElementById("clearBtn");
+
+//     addBtn.onclick = function  addingTask() {
+
+//         //document.getElementById("textField").innerHTML += mainTask;
+//         var taskKey = document.getElementById("task").value;
+//         var mainTask = document.getElementById("textField");
+
+//         //if (oldTask === null) oldTask = "";
+//         //saving the user input
+//         localStorage.setItem( taskKey,taskKey);
+//         var storedKey = localStorage.getItem(taskKey);
+//         console.log(taskKey);
+
+//         document.getElementById("textField").value += storedKey+ "\n";
+//         mainTask.textContent = localStorage.getItem(storedKey);
+//         // document.getElementById("task").value = "";
+//     }
+//     clearBTn.onclick = function clearTask() {
+//         localStorage.clear();
+//         document.getElementById("task").value = "";
+//         document.getElementById("textField").value = "";
+//     }
+// }
 // function fun(){
 // var addbtn=document.getElementById("task1").value;
 // var clearbtn=document
@@ -51,13 +74,8 @@ function onLoadEvent() {
 
 // }
 
-
-
-
-// function clearTask(){
+//function clearTask(){
 //     localStorage.clear();
 
 //     document.getElementById("task1").value=" ";
 //     document.getElementById("rightbox").value=""
-    
-// 
