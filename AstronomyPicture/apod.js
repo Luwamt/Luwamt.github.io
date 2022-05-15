b6b4e1acc0b0dbb21150473f6e3c1ddb3ca224aa
Luwam15 <80141128+Luwam15@users.noreply.github.com>
2022-05-15 00:06:00 -0500

    astronomy (fetch) added

--- AstronomyPicture/apod.js
+++ AstronomyPicture/apod.js
@@ -6,12 +6,13 @@
     function () {
     $("#view_button").click(getPicture);
     });
+
     function getPicture () {
+       let date=$("#date").val();
     $.ajax({
-    url: "https://api.nasa.gov/planetary/apod",
+    url: `https://api.nasa.gov/planetary/apod?date=${date}`,
     type: "GET",
-    data: { api_key: "DEMO_KEY",
-    date: $("date").val() },
+    data: { api_key: "tfofVD2BzaJDMUT3znaYMv06ec8AN3CR0CuiSQNY" },
     dataType: "json",
     "success": showPicture,
     "error": noPicture
@@ -20,6 +21,7 @@
     function showPicture(data) {
     $("#pic").attr("src", data.url);
     $("#title").text(data.title);
+    $("#explanation").text(data.explanation);
     };
     function noPicture(error) {
     alert(error.responseText);
